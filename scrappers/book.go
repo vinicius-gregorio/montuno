8e58c2fc8df99b34eb60b187bba864a8f2a0bce4
Vinicius Gregorio <vinicius.squadp@genial.com.vc>
2022-08-29 22:12:51 -0300

    feat: scrap books with pagination

--- scrappers/book.go
+++ scrappers/book.go
@@ -6,15 +6,27 @@ import (
 	"github.com/gocolly/colly"
 )
 
+type Book struct {
+	Title string
+	Price string
+}
+
 func StartScrapingBooks() {
 	c := colly.NewCollector(
 		colly.AllowedDomains("books.toscrape.com"),
 	)
 
-	c.OnHTML("title", func(e *colly.HTMLElement) {
-		fmt.Println(e.Text)
+	c.OnHTML(".product_pod", func(e *colly.HTMLElement) {
+		book := Book{}
+		book.Title = e.ChildAttr(".image_container img", "alt")
+		book.Price = e.ChildText(".price_color")
+		fmt.Println(book.Title, book.Price)
 	})
 
+	c.OnHTML(".next > a", func(e *colly.HTMLElement) {
+		nextPage := e.Request.AbsoluteURL(e.Attr("href"))
+		c.Visit(nextPage)
+	})
 	c.OnResponse(func(r *colly.Response) {
 		fmt.Println(r.StatusCode)
 	})
